b2852e7437871617ee52f8efeb9b52bebc14e948
Aden Tran <47045532+adeziio@users.noreply.github.com>
2021-08-27 02:18:04 +0000

    turn keep alive back on

--- main.py
+++ main.py
@@ -167,5 +167,5 @@ async def on_message(message):
           if ran_num == 1:
             await message.channel.send(get_insults(word))
 # ---------------------------------------------------------------------------------------------------------------------------
-# keep_alive()
+keep_alive()
 client.run(os.getenv('BOT_TOKEN'))
