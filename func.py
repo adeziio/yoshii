c039877c85b49147e7deec520a480c93cc8010b7
Aden Tran <47045532+adeziio@users.noreply.github.com>
2022-01-08 20:12:22 +0000

    more data massage

--- func.py
+++ func.py
@@ -130,6 +130,7 @@ def get_chatbot(searchList):
     resMsg = resMsg.replace("Aco", "yoshii")
     resMsg = resMsg.replace("yoshiibot Team", "Aden Tran")
     resMsg = resMsg.replace("Ava", "Friend")
+    resMsg = resMsg.replace("female", "male")
     return resMsg
   else:
     return ""
